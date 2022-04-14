dd37a64d29f08c869ec9d3b532652c0e53792ef1
Dennis Meelker <dennis.meelker@infi.nl>
2022-04-14 16:12:04 +0200

    Added image model

---
+++ backend/Investahoot.Model/Models/Image.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investahoot.Model.Models
+{
+    public class Image
+    {
+        private const int width = 22;
+        private const int height = 6;
+
+        private int[,] _cells;
+
+        public Image()
+        {
+            _cells = EmptyImage();
+        }
+
+        private int[,] EmptyImage()
+        {
+            var cells = new int[width, height];
+
+            for (var x = 0; x < width; x++)
+            {
+                for (var y = 0; y < height; y++)
+                {
+                    cells[x, y] = 0;
+                }
+            }
+
+            return cells;
+        }
+    }
+}
